66965d6d666a517c7ed97798c7f27a3ec371c57b
suhelkhanca <suhel.hata@gmail.com>
2025-05-27 22:44:59 +0530

    set  :  DB URI

--- eConnect/econnect.py
+++ eConnect/econnect.py
@@ -15,7 +15,7 @@ from functools import wraps
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///econnect3.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
